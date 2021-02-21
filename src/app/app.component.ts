569e294b5e5efe2b9138dff04bfe462e649e615c
atlasboris <atlasboris@gmail.com>
2021-02-21 07:29:21 +0200

    stackblitz fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Address } from './address-table/address-table.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'address-autocomplete';
   museums: string[] = [
     'Solomon R. Guggenheim Museum',
@@ -20,41 +20,7 @@ export class AppComponent implements AfterViewInit {
 
   constructor(private ref: ChangeDetectorRef) { }
 
-  ngAfterViewInit() {
-    setTimeout(() => {
-      this.museums.forEach(name => {
-        this.initTable(name);
-      }, 1000)
-    })
-  }
 
-  initTable(val: string): void {
-    let map = new google.maps.Map(document.getElementById("map") as HTMLElement);
-    let placesService = new google.maps.places.PlacesService(map);
-
-    const request = {
-      query: val,
-      fields: ['formatted_address', 'types']
-    };
-
-    placesService.findPlaceFromQuery(request, (results: any, status) => {
-
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        let resArr = results[0].formatted_address.split(",");
-        let loc: Address = {
-          street: resArr[0],
-          city: resArr[1],
-          zip: resArr[2],
-          typeOfAddress: results[0].types[0],
-          formattedAddress: results[0].formatted_address
-        }
-        this.locations.push(loc)
-      }
-      else {
-        console.log("doesn't work");
-      }
-    });
-  }
 
   getAddress(place: object) {
     this.locations.push(this.getExactAddress(place));
